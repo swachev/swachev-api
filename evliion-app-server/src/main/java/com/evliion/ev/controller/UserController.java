3a0292ed654d40738a1d6f9a805e869ed8628fb1
vidhushini.srinivasan <vidhushini.srinivasan@oracle.com>
2020-07-24 14:38:51 -0700

    undo changes

--- evliion-app-server/src/main/java/com/evliion/ev/controller/UserController.java
+++ evliion-app-server/src/main/java/com/evliion/ev/controller/UserController.java
@@ -44,7 +44,7 @@ public class UserController {
     @GetMapping("/user/me")
     @PreAuthorize("hasRole('USER')")
     public UserSummary getCurrentUser(@CurrentUser UserPrincipal currentUser) {
-        //UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getMobileNumber(), currentUser.getName());
+        //UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getName());
     	UserSummary userSummary = new UserSummary(currentUser.getId(), currentUser.getUsername(), currentUser.getName(), currentUser.getAuthorities());
         return userSummary;
     }
