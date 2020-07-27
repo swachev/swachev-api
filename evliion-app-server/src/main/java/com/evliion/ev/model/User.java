9424e6785fe31b51fe1271d9ed3affe34dc0a497
vidhushini.srinivasan <vidhushini.srinivasan@oracle.com>
2020-07-27 10:00:11 -0700

    update User Entity

--- evliion-app-server/src/main/java/com/evliion/ev/model/User.java
+++ evliion-app-server/src/main/java/com/evliion/ev/model/User.java
@@ -16,7 +16,7 @@ import java.util.Set;
 @Entity
 @Table(name = "users", uniqueConstraints = {
         @UniqueConstraint(columnNames = {
-            "mobileNumber"
+            "mobilenumber"
         }),
         @UniqueConstraint(columnNames = {
             "email"
